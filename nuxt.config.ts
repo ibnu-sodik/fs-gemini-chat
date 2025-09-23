74c03f4c63618a3c43440be5c8e4a2fdfe7fddc4
Ibnu Sodik <ibnusodik049@gmail.com>
2025-09-23 20:35:35 +0700

    feat: add Logto authentication module and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,14 +6,19 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
   css: ["~/assets/css/main.css", "vue3-toastify/dist/index.css"],
+  modules: ["@logto/nuxt"],
   vite: {
     plugins: [tailwindcss()],
   },
   runtimeConfig: {
     // Private keys (only available on server-side)
+    logtoAppSecret: process.env.NUXT_LOGTO_APP_SECRET,
+    logtoCookieEncryptionKey: process.env.NUXT_LOGTO_COOKIE_ENCRYPTION_KEY,
     // Public keys (exposed to client-side)
     public: {
       appName: process.env.APP_NAME || "Nuxt Gemini Chatbot",
+      logtoEndpoint: process.env.NUXT_LOGTO_ENDPOINT,
+      logtoAppId: process.env.NUXT_LOGTO_APP_ID,
     },
   },
 });
